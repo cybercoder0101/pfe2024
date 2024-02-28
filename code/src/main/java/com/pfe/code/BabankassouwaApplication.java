de54d2d747f0b9d575010b16801373340e6632d0
papi <ibrahimsalif375@gmail>
2024-02-28 14:16:48 +0100

    pfebase

---
+++ code/src/main/java/com/pfe/code/BabankassouwaApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabankassouwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabankassouwaApplication.class, args);
+	}
+
+}
